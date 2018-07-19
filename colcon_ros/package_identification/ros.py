8a57f4b67e22090170c98834d5c2699059acecee
Mickael Gaillard <mick.gaillard@gmail.com>
2018-07-19 19:45:18 +0200

    Fix E501
    Signed-off-by: Mickael Gaillard <mick.gaillard@gmail.com>

--- colcon_ros/package_identification/ros.py
+++ colcon_ros/package_identification/ros.py
@@ -159,7 +159,8 @@ def _get_build_type(pkg):
             'build type'.format_map(locals()))
         return None
 
-    if build_type not in ('ament_cmake', 'ament_python', 'catkin', 'cmake', 'gradle'):
+    if build_type not in ('ament_cmake', 'ament_python', 'catkin', 'cmake',
+                'gradle'):
         path = os.path.dirname(pkg.filename)
         logger.debug(
             "ROS package '{pkg.name}' in '{path}' has unknown build "
