98411fed74c31c822ae9dd2f8ba6777afa215ef8
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-12-26 16:02:40 -0800

    keep extend_cpp_with_app in case it is still used anywhere

--- colcon_ros/task/__init__.py
+++ colcon_ros/task/__init__.py
@@ -15,3 +15,35 @@ def append_app_to_cpp(env):
             if p not in cpp:
                 cpp.append(p)
         env['CMAKE_PREFIX_PATH'] = os.pathsep.join(cpp)
+
+
+def extend_cpp_with_app(args):
+    """Extend CMAKE_PREFIX_PATH with AMENT_PREFIX_PATH."""
+    ament_prefix_path = os.environ.get('AMENT_PREFIX_PATH')
+    if ament_prefix_path:
+        ament_prefix_path = ament_prefix_path.replace(
+            os.pathsep, ';')
+        if args.cmake_args is None:
+            args.cmake_args = []
+        # check if the CMAKE_PREFIX_PATH is explicitly set
+        prefix = '-DCMAKE_PREFIX_PATH='
+        for i, value in reversed(list(enumerate(args.cmake_args))):
+            if not value.startswith(prefix):
+                continue
+            # extend the last existing entry
+            existing = value[len(prefix):]
+            if existing:
+                existing = ';' + existing
+            args.cmake_args[i] = \
+                '-DCMAKE_PREFIX_PATH={ament_prefix_path}{existing}' \
+                .format_map(locals())
+            break
+        else:
+            # otherwise extend the environment variable
+            existing = os.environ.get('CMAKE_PREFIX_PATH', '')
+            if existing:
+                existing = ';' + existing.replace(
+                    os.pathsep, ';')
+            args.cmake_args.append(
+                '-DCMAKE_PREFIX_PATH={ament_prefix_path}{existing}'
+                .format_map(locals()))
