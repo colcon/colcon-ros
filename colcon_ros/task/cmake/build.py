8d61048db5960f13582b2f5c2a51e37170e3a90d
John Wason <wason@wasontech.com>
2019-06-13 14:46:04 -0400

    Add additional variables to CMake build to help packages detect ROS

--- colcon_ros/task/cmake/build.py
+++ colcon_ros/task/cmake/build.py
@@ -7,6 +7,8 @@ from colcon_core.logging import colcon_logger
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.task import TaskExtensionPoint
 from colcon_ros.task import add_app_to_cpp
+from colcon_core import __version__ as colcon_core_version
+from os import environ
 
 logger = colcon_logger.getChild(__name__)
 
@@ -27,5 +29,14 @@ class CmakeBuildTask(TaskExtensionPoint):
         # reuse CMake build task with additional logic
         extension = CmakeBuildTask_()
         extension.set_context(context=self.context)
+
+        #Add extra CMake variables to help packages detect ROS
+        if self.context.args.cmake_args is None:
+            self.context.args.cmake_args = []
+        self.context.args.cmake_args += ['-DCOLCON_VERSION:STRING=' + str(colcon_core_version)]
+        if 'ROS_VERSION' in environ:
+            self.context.args.cmake_args += ['-DCOLCON_ROS_VERSION:STRING=' + environ['ROS_VERSION']]
+        if 'ROS_DISTRO' in environ:
+            self.context.args.cmake_args += ['-DCOLCON_ROS_DISTRO:STRING=' + environ['ROS_DISTRO']]
 
         return await extension.build(environment_callback=add_app_to_cpp)
