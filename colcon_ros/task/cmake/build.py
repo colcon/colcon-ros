88663a0512486ae497932745f88540cf57d1792c
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-08-26 15:42:59 -0700

    add PKG_CONFIG_PATH for cmake and catkin packages (#72)

--- colcon_ros/task/cmake/build.py
+++ colcon_ros/task/cmake/build.py
@@ -1,12 +1,15 @@
 # Copyright 2016-2018 Dirk Thomas
 # Licensed under the Apache License, Version 2.0
 
+from pathlib import Path
 
 from colcon_cmake.task.cmake.build import CmakeBuildTask as CmakeBuildTask_
+from colcon_core.environment import create_environment_scripts
 from colcon_core.logging import colcon_logger
 from colcon_core.plugin_system import satisfies_version
 from colcon_core.task import TaskExtensionPoint
 from colcon_ros.task import add_app_to_cpp
+from colcon_ros.task.cmake import create_pkg_config_path_environment_hooks
 
 logger = colcon_logger.getChild(__name__)
 
@@ -28,4 +31,12 @@ class CmakeBuildTask(TaskExtensionPoint):
         extension = CmakeBuildTask_()
         extension.set_context(context=self.context)
 
-        return await extension.build(environment_callback=add_app_to_cpp)
+        rc = await extension.build(environment_callback=add_app_to_cpp)
+
+        additional_hooks = create_pkg_config_path_environment_hooks(
+            Path(args.install_base), self.context.pkg.name)
+
+        create_environment_scripts(
+            self.context.pkg, args, additional_hooks=additional_hooks)
+
+        return rc
